6b21075c915a298c12c256a851810ae3dd057f1d
Johnson Joseph <jhonsonj88@gmail.com>
2020-01-06 23:02:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ document.addEventListener("keydown", function(e) {
 });
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
 
-  if (right > 0) {
-    dodger.style.right = `${right - 1}px`;
-  }
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
 }
 
 document.addEventListener("keydown", function(e) {
-  if (e.key === "ArrowRight") {
-    moveDodgerRight();
-  }
-});
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });
